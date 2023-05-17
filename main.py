ef1e8ac7a5719e2c332f367b2c00bf9e2009b34d
Ivan <inrusakov@edu.hse.ru>
2023-05-17 18:58:35 +0300

    log rework, commands rework, commands handling rework

--- main.py
+++ main.py
@@ -2,20 +2,32 @@
 import telebot, json, pickle
 # Для указания Типов и создания внутренней клавиатуры.
 from telebot import types
+# Функция для получения строк из документа с оригинальным разделением.
+def read_and_combine_lines(file_path):
+    with open(file_path, 'r', encoding="utf-8") as file:
+        lines = file.readlines()
+        combined_lines = ''.join(lines)
+    return combined_lines
+
 # Функция для получения строки из документа.
 def get_string_from_file(path):
     with open(path, "r", encoding="utf-8") as f:
         str_data = f.readline().strip()
     return str_data
+
 # Подключение бота по токену.
 bot = telebot.TeleBot(get_string_from_file("token.txt"))
-# Времменная база данных для хранения информации о прогрессе пользователей.
-users = {}
-# Функция для добавления/обновления информации о пользователя.
-def update_user_data(chat_id, user):
-    users[chat_id] = {user}
-    log_users("users_log.txt")
 
+# Времменная база данных для хранения информации о прогрессе пользователей.
+user_dict = {}
+
+# Функция для добавления/обновления информации о пользователя.
+def log_users(filename="users_log.txt"):
+    with open(filename, "a") as file:
+        for user in user_dict.values():
+            file.write(str(user) + "\n")
+
+# Класс хранения данных о пользователе.
 class User:
     def __init__(self, user_id, current_book, pages, book_progress, condition):
         self.user_id = user_id
@@ -26,10 +38,6 @@ class User:
 
     def __str__(self):
         return f"User ID: {self.user_id}, Current Book: {self.current_book}, Pages: {self.pages}, Book Progress: {self.book_progress}, condition: {self.condition}%"
-
-
-# Создание временной базы данных для пользователей.
-user_dict = {}
 
 # Функция обработки команды start.
 @bot.message_handler(commands=['start'])
@@ -42,13 +50,13 @@ def start_command(message):
 
     user = message.from_user.username
     chat_id = message.chat.id
-    update_user_data(chat_id, user)
+    log_users()
 
     user = User(chat_id, "", 0, 0, '0')
     user_dict[user.user_id] = user
     
     start_video = open(r'C:\Users\Rus-P\Desktop\Work\HSE\HabitBot\HabitTrackerHSE\start.mp4', 'rb')
-    bot.send_message(message.chat.id, text=get_string_from_file("commands\startcommand.txt").format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text=read_and_combine_lines("commands\startcommand.txt").format(message.from_user), reply_markup=markup)
     bot.send_video(message.chat.id, start_video, timeout=10)
 
 # Функция обработки команды setatarget.
@@ -62,7 +70,7 @@ def start_command(message):
 
     user_dict[message.chat.id].condition = 'STB'
 
-    bot.send_message(message.chat.id, text=get_string_from_file("commands\setatarget.txt").format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text=read_and_combine_lines("commands\setatarget.txt").format(message.from_user), reply_markup=markup)
 
 # Функция обработки команды addpages.
 @bot.message_handler(commands=['addpages'])
@@ -75,7 +83,7 @@ def start_command(message):
 
     user_dict[message.chat.id].condition = 'APB'
 
-    bot.send_message(message.chat.id, text=get_string_from_file("commands\\addpages.txt").format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text=read_and_combine_lines("commands\\addpages.txt").format(message.from_user), reply_markup=markup)
 
 # Функция обработки команды progress.
 @bot.message_handler(commands=['progress'])
@@ -87,12 +95,11 @@ def start_command(message):
     markup.add(btn1)
 
     bot.send_message(message.chat.id, 
-                     text=get_string_from_file("commands\progress.txt")
+                     text=read_and_combine_lines("commands\progress.txt")
                      .format(message.from_user,
                               user_dict[message.chat.id].current_book,
                               user_dict[message.chat.id].book_progress,
                               user_dict[message.chat.id].pages), reply_markup=markup)
-
 
 # Функция обработки команды help.
 @bot.message_handler(commands=['info'])
@@ -105,7 +112,7 @@ def help_command(message):
     btn3 = types.KeyboardButton("/progress")
     markup.add(btn1, btn2, btn3)
 
-    bot.send_message(message.chat.id, text=get_string_from_file("commands\infocommand.txt").format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text=read_and_combine_lines("commands\infocommand.txt"), reply_markup=markup)
 
 # Функция обработки команды donate.
 @bot.message_handler(commands=['donate'])
@@ -119,7 +126,8 @@ def help_command(message):
     bot.send_message(message.chat.id,
                      text="{0.first_name}, Спасибо за поддержку!"
                      .format(message.from_user), reply_markup=markup)
-    
+
+# Функция обработки сообщений и проверки состояния диалога для пользователей.    
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def check_answer(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -131,7 +139,7 @@ def check_answer(message):
     if(user_dict[message.chat.id].condition == 'STB'):
         bot.send_message(message.chat.id,
                      text="Название введено: {0.text}, теперь введите количество страниц."
-                     .format(message), reply_markup=markup)
+                     .format(message))
         user_dict[message.chat.id].current_book = message.text
         user_dict[message.chat.id].condition = 'STN'
         return
@@ -145,9 +153,10 @@ def check_answer(message):
         else:
             bot.send_message(message.chat.id,
                      text="Неправильный формат ввода, введите число: {0.text}"
-                     .format(message), reply_markup=markup)
+                     .format(message))
             return
         user_dict[message.chat.id].condition = '0'
+        log_users()
         return
 
     if(user_dict[message.chat.id].condition == 'APB'):
@@ -162,13 +171,13 @@ def check_answer(message):
                      .format(message), reply_markup=markup)
             return
         user_dict[message.chat.id].condition = '0'
+        log_users()
         return
 
     else:
         bot.send_message(message.chat.id,
                      text="{0.first_name}, Введена неправильная команда"
                      .format(message.from_user), reply_markup=markup)
-
 
 # Функция для логгирования всех пришедших боту обращений.
 def log_commands(message):
@@ -180,12 +189,6 @@ def log_commands(message):
     user = message.from_user.username
     chat_id = message.chat.id
     return "Message: " + text + " from @" + user + " chat ID: " + str(chat_id)
-
-def log_users(path):
-    with open(path, "a", encoding="utf-8") as f:
-        for key in users:
-            f.write(str(key) + " " + str(list(users[key])))
-        f.close()
 
 # Команда которая говорит боту, что нужно забирать полученные сообщения.
 bot.infinity_polling()
