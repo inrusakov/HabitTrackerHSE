0a02ea10b632360c13afae54b7f7b36264af1861
Ivan <inrusakov@edu.hse.ru>
2023-05-22 17:57:25 +0300

    fix + db

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import datetime
 import pytz
 import logging
 import threading
+import os
+import json
 # Для указания Типов и создания внутренней клавиатуры.
 from telebot import types
 
@@ -21,6 +23,30 @@ def get_string_from_file(path):
         str_data = f.readline().strip()
     return str_data
 
+# Функция для записи данных в файл.
+def save_data_to_file(file_path, user_dict):
+    with open(file_path, "w") as f:
+        json.dump(user_dict, f, default=lambda o: o.__json__(), indent=4)
+    moscow_tz = pytz.timezone('Europe/Moscow')
+    now = datetime.datetime.now(moscow_tz)
+    print("Time: " + str(now.hour)+":"+str(now.minute) +" Данные сохранены в файл.")
+
+# Функция для чтения данных пользователей из файла.
+def load_data_from_file(file_path):
+    with open(file_path, "r") as f:
+        file_content = f.read()
+        if not file_content:
+            print("Файл пуст.")
+            return {}
+        user_dict = json.loads(file_content)
+        new_user_dict = {}
+        for user_id, user_data in user_dict.items():
+            new_user_dict[int(user_id)] = User(**user_data)
+        moscow_tz = pytz.timezone('Europe/Moscow')
+        now = datetime.datetime.now(moscow_tz)
+        print("Time: " + str(now.hour)+":"+str(now.minute) +" Данные загружены из файла.")
+        return new_user_dict
+
 # Подключение бота по токену.
 bot = telebot.TeleBot(get_string_from_file("token.txt"))
 
@@ -30,13 +56,24 @@ user_dict = {}
 # Класс хранения данных о пользователе.
 class User:
     def __init__(self, user_id, current_book, pages, book_progress, condition, hour=None, minute=None):
-        self.user_id = user_id
-        self.current_book = current_book
-        self.pages = pages
-        self.book_progress = book_progress
-        self.condition = condition
-        self.hour = hour
-        self.minute = minute
+        self.user_id = int(user_id)
+        self.current_book = str(current_book)
+        self.pages = int(pages)
+        self.book_progress = int(book_progress)
+        self.condition = str(condition)
+        self.hour = int(hour) if hour is not None else None
+        self.minute = int(minute) if minute is not None else None
+
+    def __json__(self):
+        return {
+            "user_id": self.user_id,
+            "current_book": self.current_book,
+            "pages": self.pages,
+            "book_progress": self.book_progress,
+            "condition": self.condition,
+            "hour": self.hour,
+            "minute": self.minute
+        }
 
     def __str__(self):
         return f"User ID: {self.user_id}, Current Book: {self.current_book}, Pages: {self.pages}, Book Progress: {self.book_progress}, Condition: {self.condition}, Time: {self.hour:02d}:{self.minute:02d}"
@@ -60,59 +97,79 @@ def start_command(message):
     user = message.from_user.username
     chat_id = message.chat.id
     log_users()
-
+    
     user = User(chat_id, "", 0, 0, '0', 0, 0)
     user_dict[user.user_id] = user
     
-    with open(r'C:\Users\Rus-P\Desktop\Work\HSE\HabitBot\HabitTrackerHSE\start.mp4', 'rb') as start_video:
-        bot.send_message(message.chat.id, text=read_and_combine_lines("commands\startcommand.txt").format(message.from_user), reply_markup=markup)
+    save_data_to_file("user_data.json", user_dict)
+    with open(r'start.mp4', 'rb') as start_video:
+        bot.send_message(message.chat.id, text=read_and_combine_lines("commands"+os.sep+"startcommand.txt").format(message.from_user), reply_markup=markup)
         bot.send_video(message.chat.id, start_video, timeout=10)
 
 # Функция обработки команды setatarget.
 @bot.message_handler(commands=['setatarget'])
-def start_command(message):
+def setatarget_command(message):
     print(log_commands(message))
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("/info")
-    markup.add(btn1)
+    user_dict = load_data_from_file("user_data.json")
 
-    user_dict[message.chat.id].condition = 'STB'
+    try:
+        user_dict[message.chat.id].condition = 'STB'
+    except:
+        bot.send_message(message.chat.id,
+                     text="{0.first_name}, бот перезапускался, для корректной работы начни с команды /start."
+                     .format(message.from_user))
+        return
 
-    bot.send_message(message.chat.id, text=read_and_combine_lines("commands\setatarget.txt").format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text=read_and_combine_lines("commands"+os.sep+"setatarget.txt").format(message.from_user))
+    save_data_to_file("user_data.json", user_dict)
 
 # Функция обработки команды addpages.
 @bot.message_handler(commands=['addpages'])
-def start_command(message):
+def addpages_command(message):
     print(log_commands(message))
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("/info")
-    markup.add(btn1)
+    user_dict = load_data_from_file("user_data.json")
 
-    user_dict[message.chat.id].condition = 'APB'
+    try:
+        user_dict[message.chat.id].condition = 'APB'
+    except:
+        bot.send_message(message.chat.id,
+                     text="{0.first_name}, бот перезапускался, для корректной работы начни с команды /start."
+                     .format(message.from_user))
+        return
 
-    bot.send_message(message.chat.id, text=read_and_combine_lines("commands\\addpages.txt").format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text=read_and_combine_lines("commands"+os.sep+"addpages.txt").format(message.from_user))
+    save_data_to_file("user_data.json", user_dict)
 
 # Функция обработки команды progress.
 @bot.message_handler(commands=['progress'])
-def start_command(message):
+def progress_command(message):
     print(log_commands(message))
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("/info")
     markup.add(btn1)
 
-    bot.send_message(message.chat.id, 
-                     text=read_and_combine_lines("commands\progress.txt")
+    user_dict = load_data_from_file("user_data.json")
+
+    try:
+        bot.send_message(message.chat.id, 
+                     text=read_and_combine_lines("commands"+os.sep+"progress.txt")
                      .format(message.from_user,
                               user_dict[message.chat.id].current_book,
                               user_dict[message.chat.id].book_progress,
                               user_dict[message.chat.id].pages), reply_markup=markup)
+    except:
+        bot.send_message(message.chat.id,
+                     text="{0.first_name}, бот перезапускался, для корректной работы начни с команды /start."
+                     .format(message.from_user))
+        return
+    save_data_to_file("user_data.json", user_dict)
 
 # Функция обработки команды help.
 @bot.message_handler(commands=['info'])
-def help_command(message):
+def info_command(message):
     print(log_commands(message))
     
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -121,11 +178,11 @@ def help_command(message):
     btn3 = types.KeyboardButton("/progress")
     markup.add(btn1, btn2, btn3)
 
-    bot.send_message(message.chat.id, text=read_and_combine_lines("commands\infocommand.txt"), reply_markup=markup)
+    bot.send_message(message.chat.id, text=read_and_combine_lines("commands"+os.sep+"infocommand.txt"), reply_markup=markup)
 
 # Функция обработки команды donate.
 @bot.message_handler(commands=['donate'])
-def help_command(message):
+def donate_command(message):
     print(log_commands(message))
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -144,63 +201,80 @@ def check_answer(message):
     btn2 = types.KeyboardButton("/addpages")
     btn3 = types.KeyboardButton("/progress")
     markup.add(btn1, btn2, btn3)
-
-    if(user_dict[message.chat.id].condition == 'STB'):
-        bot.send_message(message.chat.id,
-                     text="Название введено: {0.text}, теперь введите количество страниц."
-                     .format(message))
-        user_dict[message.chat.id].current_book = message.text
-        user_dict[message.chat.id].condition = 'STN'
-        return
-
-    if(user_dict[message.chat.id].condition == 'STN'):
-        if(message.text.isnumeric()):
-            bot.send_message(message.chat.id,
-                     text="Введено количество страниц: {0.text}. \nТеперь введи время в которое я буду напоминать тебе о прочитанных страницах. \nПример ввода: 17:00"
-                     .format(message))
-            user_dict[message.chat.id].pages = int(message.text)
-            user_dict[message.chat.id].condition = 'STT'
-        else:
-            bot.send_message(message.chat.id,
-                     text="Неправильный формат ввода, введите число: {0.text}"
-                     .format(message))
-            return
-        log_users()
-        return
     
-    if(user_dict[message.chat.id].condition == 'STT'):
-        try:
-            user_dict[message.chat.id].set_time(message.text)
-        except ValueError:
-            bot.send_message(message.chat.id,
-                     text="Неверный формат времени. Используйте формат «ЧЧ:ММ» (24-часовой формат).")
-            return
-        bot.send_message(message.chat.id,
-                     text="Введено время: {0.text}, отлично теперь я буду каждый день напоминать тебе почитать книгу в это время"
-                     .format(message), reply_markup=markup)
-        user_dict[message.chat.id].condition = '0'
-        log_users()
-        return
+    user_dict = load_data_from_file("user_data.json")
 
-    if(user_dict[message.chat.id].condition == 'APB'):
-        if(message.text.isnumeric()):
-            user_dict[message.chat.id].book_progress += int(message.text)
+    try:
+        if(user_dict[message.chat.id].condition == 'STB'):
             bot.send_message(message.chat.id,
-                     text="{0.text} страниц добавлено к вашиму прогрессу, текущий прогресс: {1}"
-                     .format(message, user_dict[message.chat.id].book_progress), reply_markup=markup)
+                        text="Название введено: {0.text}, теперь введите количество страниц."
+                        .format(message))
+            user_dict[message.chat.id].current_book = message.text
+            user_dict[message.chat.id].condition = 'STN'
+            save_data_to_file("user_data.json", user_dict)
+            return
+
+        if(user_dict[message.chat.id].condition == 'STN'):
+            if(message.text.isnumeric()):
+                bot.send_message(message.chat.id,
+                        text="Введено количество страниц: {0.text}. \nТеперь введи время в которое я буду напоминать тебе о прочитанных страницах. \nПример ввода: 17:00"
+                        .format(message))
+                user_dict[message.chat.id].pages = int(message.text)
+                user_dict[message.chat.id].condition = 'STT'
+            else:
+                bot.send_message(message.chat.id,
+                        text="Неправильный формат ввода, введите число: {0.text}"
+                        .format(message))
+                return
+            log_users()
+            save_data_to_file("user_data.json", user_dict)
+            return
+        
+        if(user_dict[message.chat.id].condition == 'STT'):
+            try:
+                user_dict[message.chat.id].set_time(message.text)
+            except ValueError:
+                bot.send_message(message.chat.id,
+                        text="Неверный формат времени. Используйте формат «ЧЧ:ММ» (24-часовой формат).")
+                return
+            bot.send_message(message.chat.id,
+                        text="Введено время: {0.text}, отлично теперь я буду каждый день напоминать тебе почитать книгу в это время"
+                        .format(message), reply_markup=markup)
+            user_dict[message.chat.id].condition = '0'
+            log_users()
+            save_data_to_file("user_data.json", user_dict)
+            return
+
+        if(user_dict[message.chat.id].condition == 'APB'):
+            if(message.text.isnumeric()):
+                user_dict[message.chat.id].book_progress += int(message.text)
+                if(user_dict[message.chat.id].book_progress >= user_dict[message.chat.id].pages):
+                    bot.send_message(message.chat.id,
+                        text="{0.text} страниц добавлено к вашиму прогрессу, поздравляю с прочтением книги! Введи команду /setatarget и подолжай читать."
+                        .format(message), reply_markup=markup)
+                else:
+                    bot.send_message(message.chat.id,
+                            text="{0.text} страниц добавлено к вашиму прогрессу, текущий прогресс: {1}"
+                            .format(message, user_dict[message.chat.id].book_progress), reply_markup=markup)
+            else:
+                bot.send_message(message.chat.id,
+                        text="Неправильный формат ввода, введите число: {0.text}"
+                        .format(message), reply_markup=markup)
+                return
+            user_dict[message.chat.id].condition = '0'
+            log_users()
+            save_data_to_file("user_data.json", user_dict)
+            return
+
         else:
             bot.send_message(message.chat.id,
-                     text="Неправильный формат ввода, введите число: {0.text}"
-                     .format(message), reply_markup=markup)
-            return
-        user_dict[message.chat.id].condition = '0'
-        log_users()
-        return
-
-    else:
+                        text="{0.first_name}, Введена неправильная команда"
+                        .format(message.from_user), reply_markup=markup)
+    except:
         bot.send_message(message.chat.id,
-                     text="{0.first_name}, Введена неправильная команда"
-                     .format(message.from_user), reply_markup=markup)
+                     text="{0.first_name}, бот перезапускался, для корректной работы начни с команды /start."
+                     .format(message.from_user))
+        return
 
 # Функция для логгирования всех пришедших боту обращений.
 def log_commands(message):
@@ -211,7 +285,9 @@ def log_commands(message):
         text = message.text
     user = message.from_user.username
     chat_id = message.chat.id
-    return "Message: " + text + " from @" + user + " chat ID: " + str(chat_id)
+    moscow_tz = pytz.timezone('Europe/Moscow')
+    now = datetime.datetime.now(moscow_tz)
+    return "Time: " + str(now.hour)+":"+str(now.minute) +" Message: " + text + " from @" + user + " chat ID: " + str(chat_id)
 
 # Функция для добавления/обновления информации о пользователя.
 def log_users(filename="users_log.txt"):
@@ -223,6 +299,7 @@ def log_users(filename="users_log.txt"):
 async def check_user_times():
     moscow_tz = pytz.timezone('Europe/Moscow')
     while True:
+        user_dict = load_data_from_file("user_data.json")
         now = datetime.datetime.now(moscow_tz)
         for user in user_dict.values():
             if user.hour == now.hour and user.minute == now.minute:
